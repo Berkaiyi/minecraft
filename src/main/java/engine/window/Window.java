428fc43c89fdfe47fe8a6865de4bbed87d3f6e3a
mehranalizada <alizadamehran@gmail.com>
2025-12-22 16:58:08 +0100

    framebufferSizeCallback

--- src/main/java/engine/window/Window.java
+++ src/main/java/engine/window/Window.java
@@ -1,15 +1,21 @@
 package engine.window;
 
 import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWFramebufferSizeCallback;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL11;
 
+import java.nio.IntBuffer;
+import org.lwjgl.system.MemoryStack;
+
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.glfw.GLFW.*;
 
 public class Window {
     private long window;
     private final String title;
     private final int width, height;
+    private GLFWFramebufferSizeCallback fbCallback;
 
     public Window(String title, int width, int height) {
         this.title = title;
@@ -39,7 +45,15 @@ public class Window {
         GLFW.glfwShowWindow(window);
 
         GL.createCapabilities();
-        glViewport(0, 0, width, height);
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            IntBuffer fbW = stack.mallocInt(1);
+            IntBuffer fbH = stack.mallocInt(1);
+            glfwGetFramebufferSize(window, fbW, fbH);
+            glViewport(0, 0, fbW.get(0), fbH.get(0));
+        }
+        fbCallback = glfwSetFramebufferSizeCallback(window, (win, fbWidth, fbHeight) -> {
+            glViewport(0, 0, fbWidth, fbHeight);
+        });
     }
 
     public long getHandle() {
@@ -72,6 +86,7 @@ public class Window {
     }
 
     public void destroy() {
+        if (fbCallback != null) { fbCallback.free(); }
         GLFW.glfwDestroyWindow(window);
         GLFW.glfwTerminate();
     }
